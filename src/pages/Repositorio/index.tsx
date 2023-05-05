a0ec6a9580e60b7a849b0b8837f01edd24a71709
gabriel <gabriel.gd7@outlook.com>
2023-05-05 17:44:51 -0300

    issues paginacao

--- src/pages/Repositorio/index.tsx
+++ src/pages/Repositorio/index.tsx
@@ -1,14 +1,23 @@
 import { ReactElement, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { FaArrowLeft } from "react-icons/fa";
 import api from "../../services/api";
-import { Container } from "./styles";
-import { TypeRepositorio } from "../../types/types";
+import {
+  Container,
+  Owner,
+  Loading,
+  BackButton,
+  IssuesList,
+  PageActions,
+} from "./styles";
+import { TypeRepositorio, TypeIssue } from "../../types/types";
 
 const Repositorio = (): ReactElement => {
   const { name } = useParams();
-  const [repositorio, setRepositorio] = useState<TypeRepositorio | null>();
-  const [issues, setIssues] = useState({});
+  const [repositorio, setRepositorio] = useState<TypeRepositorio | null>(null);
+  const [issues, setIssues] = useState<TypeIssue[] | null>(null);
   const [loading, setLoading] = useState<boolean>(true);
+  const [page, setPage] = useState<number>(1);
 
   useEffect(() => {
     async function load() {
@@ -29,9 +38,83 @@ const Repositorio = (): ReactElement => {
 
     load();
   }, [name]);
+
+  useEffect(() => {
+    async function loadIssues() {
+      const response = await api.get(`/repos/${name}/issues`, {
+        params: {
+          state: "open",
+          page,
+          per_page: 5,
+        },
+      });
+
+      setIssues(response.data);
+    }
+    loadIssues();
+  }, [page, name]);
+
+  const handlePage = (action: string): void => {
+    setPage((prev) => (action === "prev" ? prev - 1 : prev + 1));
+  };
+
+  if (loading) {
+    return (
+      <Loading>
+        <h1>carregando...</h1>
+      </Loading>
+    );
+  }
+
   return (
     <Container>
-      <h1></h1>
+      <BackButton to="/">
+        <FaArrowLeft color="#000" size={35} />
+      </BackButton>
+      <Owner>
+        {repositorio && (
+          <>
+            <img
+              src={repositorio.owner.avatar_url}
+              alt={repositorio.owner.login}
+            />
+            <h1>{repositorio.name}</h1>
+            <p>{repositorio.description}</p>
+          </>
+        )}
+      </Owner>
+      <IssuesList>
+        {issues &&
+          issues.map((i) => (
+            <li key={i.id}>
+              <img src={i.user.avatar_url} alt={i.user.login} />
+              <div>
+                <strong>
+                  <a href={i.html_url}>{i.title}</a>
+                  {i.labels.length > 0 &&
+                    i.labels.map((label) => (
+                      <span key={label.id}>{label.name}</span>
+                    ))}
+                </strong>
+
+                <p>{i.user.login}</p>
+              </div>
+            </li>
+          ))}
+      </IssuesList>
+      <PageActions>
+        <button
+          type="button"
+          onClick={() => handlePage("prev")}
+          disabled={page < 2 ? true : false}
+        >
+          Voltar
+        </button>
+
+        <button type="button" onClick={() => handlePage("next")}>
+          Proxima
+        </button>
+      </PageActions>
     </Container>
   );
 };
--- src/types/types.ts
+++ src/types/types.ts
@@ -131,3 +131,73 @@ export interface TypeRepositorio extends TypeNameRepositorio {
   network_count: number;
   subscribers_count: number;
 }
+
+const prevIssue = {
+  url: "https://api.github.com/repos/angular/angular/issues/50168",
+  repository_url: "https://api.github.com/repos/angular/angular",
+  labels_url:
+    "https://api.github.com/repos/angular/angular/issues/50168/labels{/name}",
+  comments_url:
+    "https://api.github.com/repos/angular/angular/issues/50168/comments",
+  events_url:
+    "https://api.github.com/repos/angular/angular/issues/50168/events",
+  html_url: "https://github.com/angular/angular/issues/50168",
+  id: 1697980637,
+  node_id: "I_kwDOAXExC85lNSDd",
+  number: 50168,
+  title:
+    "Input properties cannot be unset when using exactOptionalPropertyTypes",
+  user: {
+    login: "dechamps",
+    id: 866859,
+    node_id: "MDQ6VXNlcjg2Njg1OQ==",
+    avatar_url: "https://avatars.githubusercontent.com/u/866859?v=4",
+    gravatar_id: "",
+    url: "https://api.github.com/users/dechamps",
+    html_url: "https://github.com/dechamps",
+    followers_url: "https://api.github.com/users/dechamps/followers",
+    following_url:
+      "https://api.github.com/users/dechamps/following{/other_user}",
+    gists_url: "https://api.github.com/users/dechamps/gists{/gist_id}",
+    starred_url: "https://api.github.com/users/dechamps/starred{/owner}{/repo}",
+    subscriptions_url: "https://api.github.com/users/dechamps/subscriptions",
+    organizations_url: "https://api.github.com/users/dechamps/orgs",
+    repos_url: "https://api.github.com/users/dechamps/repos",
+    events_url: "https://api.github.com/users/dechamps/events{/privacy}",
+    received_events_url:
+      "https://api.github.com/users/dechamps/received_events",
+    type: "User",
+    site_admin: false,
+  },
+  labels: [{ id: 1, name: "aaa" }],
+  state: "open",
+  locked: false,
+  assignee: null,
+  assignees: [],
+  milestone: null,
+  comments: 0,
+  created_at: "2023-05-05T17:53:15Z",
+  updated_at: "2023-05-05T17:54:03Z",
+  closed_at: null,
+  author_association: "NONE",
+  active_lock_reason: null,
+  body: "### Which @angular/* package(s) are the source of the bug?\r\n\r\ncore\r\n\r\n### Is this a regression?\r\n\r\nNo\r\n\r\n### Description\r\n\r\nIf the `exactOptionalPropertyTypes` TS config option is set, then it becomes impossible to unset an input property through a binding (or at least not without working around the TS type system).\r\n\r\nFor example, given the following:\r\n\r\n```ts\r\n@Component({\r\n  selector: 'child-component',\r\n})\r\nclass ChildComponent {\r\n  @Input() someProperty?: string;\r\n}\r\n\r\n@Component({\r\n    template: `<child-component [someProperty]=\"someValue\"></child-component>`,\r\n})\r\nclass ParentComponent {\r\n  someValue?: string;\r\n}\r\n```\r\n\r\nIf the `exactOptionalPropertyTypes` TS config option is set, the above will fail to compile with `error TS2412: Type 'string | undefined' is not assignable to type 'string' with 'exactOptionalPropertyTypes: true'`.\r\n\r\nOne random example (out of many, I'd presume) where this becomes a real problem is when attempting to clear the `fragment` property of a [`routerLink` directive](https://angular.io/api/router/RouterLink). There is no clean way to do this (that I know of) if `exactOptionalPropertyTypes` is enabled. (Setting the property to the empty string is not equivalent, as that will still result in a spurious `#` character being added to the href.) The below reproducer demonstrates this particular problem.\r\n\r\nThe only way out of this is to work around the type system by [coercing the value to non-null](https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#non-null-assertion-operator-postfix-):\r\n\r\n```html\r\n<child-component [someProperty]=\"someValue!\"></child-component>\r\n```\r\n\r\nBut note that [this is still not quite correct](https://stackoverflow.com/questions/14967535/delete-a-x-vs-a-x-undefined), as it results in the property being set to `undefined`, as opposed to being unset (in the `delete` operator sense) - and in fact this is precisely the mistake `exactOptionalPropertyTypes` is designed to prevent. Fortunately most child components won't care as they typically don't make the distinction between an absent property and one that is set to `undefined`.\r\n\r\n### Please provide a link to a minimal reproduction of the bug\r\n\r\nhttps://codesandbox.io/p/sandbox/angular-router-fragment-uo79rv\r\n\r\n### Please provide the exception or error you saw\r\n\r\n```\r\nError: src/app/app.component.html:12:23 - error TS2412: Type 'string | undefined' is not assignable to type 'string' with 'exactOptionalPropertyTypes: true'. Consider adding 'undefined' to the type of the target.\r\n  Type 'undefined' is not assignable to type 'string'.\r\n```\r\n\r\n\r\n### Please provide the environment you discovered this bug in (run `ng version`)\r\n\r\n```\r\nngular CLI: 15.2.1\r\nNode: 16.17.0\r\nPackage Manager: npm 8.15.0\r\nOS: linux x64\r\n\r\nAngular: 15.2.1\r\n... animations, cli, common, compiler, compiler-cli, core, forms\r\n... language-service, platform-browser, platform-browser-dynamic\r\n... router\r\n\r\nPackage                         Version\r\n---------------------------------------------------------\r\n@angular-devkit/architect       0.1502.1\r\n@angular-devkit/build-angular   15.2.1\r\n@angular-devkit/core            15.2.1\r\n@angular-devkit/schematics      15.2.1\r\n@schematics/angular             15.2.1\r\nrxjs                            7.6.0\r\ntypescript                      4.8.2\r\n```",
+  reactions: {
+    url: "https://api.github.com/repos/angular/angular/issues/50168/reactions",
+    total_count: 0,
+    "+1": 0,
+    "-1": 0,
+    laugh: 0,
+    hooray: 0,
+    confused: 0,
+    heart: 0,
+    rocket: 0,
+    eyes: 0,
+  },
+  timeline_url:
+    "https://api.github.com/repos/angular/angular/issues/50168/timeline",
+  performed_via_github_app: null,
+  state_reason: null,
+};
+
+export type TypeIssue = typeof prevIssue;
